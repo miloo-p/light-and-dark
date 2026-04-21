8e787d2e6fef63dbf0be3399964220797c919871
Timo <timoboen@gmail.com>
2026-04-22 00:05:53 +0200

    refrac: implement addToMap & addObjectsToMap

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,7 +1,7 @@
 class World {
   shadowCharacter = new ShadowCharacter();
   lightCharacter = new LightCharacter();
-  enemiesStomps = [new EnemyStomp(), new EnemyStomp(), new EnemyStomp()];
+  enemyStomps = [new EnemyStomp(), new EnemyStomp(), new EnemyStomp()];
 
   canvas;
   ctx;
@@ -16,36 +16,27 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.drawImage(
-      this.shadowCharacter.img,
-      this.shadowCharacter.x,
-      this.shadowCharacter.y,
-      this.shadowCharacter.width,
-      this.shadowCharacter.height,
-    );
+    this.addToMap(this.shadowCharacter);
 
-    this.enemiesStomps.forEach((stomp) => {
-      this.ctx.drawImage(stomp.img, stomp.x, stomp.y, stomp.width, stomp.height);
-    });
+    this.addObjectsToMap(this.enemyStomps);
 
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
     });
   }
-}
-
-/*     this.ctx.drawImage(
-      this.enemies[0].img,
-      this.enemies[0].x,
-      this.enemies[0].y,
-      this.enemies[0].width,
-      this.enemies[0].height,
-    );
+  addObjectsToMap(objects) {
+    objects.forEach((obj) => {
+      this.addToMap(obj);
+    });
+  }
+  addToMap(MovableObject) {
     this.ctx.drawImage(
-      this.enemies[1].img,
-      this.enemies[1].x,
-      this.enemies[1].y,
-      this.enemies[1].width,
-      this.enemies[1].height,
-    ); */
+      MovableObject.img,
+      MovableObject.x,
+      MovableObject.y,
+      MovableObject.width,
+      MovableObject.height,
+    );
+  }
+}
